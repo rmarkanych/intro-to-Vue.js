56f345fff075b40c296efc1b91c3832b163375ca
Viktor Romanyuk <v.romanyuk@softonix.org>
2022-11-14 12:56:08 +0200

    section-6-vue-part-2-end

--- src/components/ContactItem.vue
+++ src/components/ContactItem.vue
@@ -1,78 +1,96 @@
 <template>
-  <div class="rounded-lg bg-white shadow relative">
-    <div class="p-6 pb-2">
-      <div class="flex">
-        <div class="flex-grow text-sm truncate">
-          <template v-if="editMode">
-            <input
-              ref="inputRef"
-              v-model="localContact.name"
-              type="text"
-              class="block font-medium w-full"
-            >
-            <input v-model="localContact.description" type="text" class="block mt-1 text-gray w-full">
-          </template>
-
-          <template v-else>
-            <p class="font-medium">{{ contact.name }}</p>
-            <p class="text-gray mt-1 truncate">
-              {{ contact.description }}
-            </p>
-          </template>
-        </div>
-        <img
-          class="w-[40px] h-[40px] object-cover ml-2 rounded-full shrink-0"
-          :src="contact.image" alt="contact-logo"
-        >
-      </div>
-      <div class="flex justify-end mt-2 gap-2">
+  <Card>
+    <div class="flex">
+      <div class="flex-grow text-sm truncate" @click.stop>
         <template v-if="editMode">
-          <span
-            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="editMode = false"
-          >Cancel</span>
-
-          <span
-            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="onSave"
-          >Save</span>
+          <input
+            ref="inputRef"
+            v-model="localContact.name"
+            type="text"
+            class="block font-medium w-full"
+          >
+          <input v-model="localContact.description" type="text" class="block mt-1 text-gray w-full">
         </template>
 
         <template v-else>
-          <span
-            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="triggerEditMode"
-          >Edit</span>
-
-          <span
-            class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="$emit('delete')"
-          >Delete</span>
+          <p class="font-medium cursor-text">{{ contact.name }}</p>
+          <p class="text-gray cursor-text mt-1 truncate">
+            {{ contact.description }}
+          </p>
         </template>
       </div>
+
+      <div
+        class="flex items-center justify-center w-[40px] h-[40px] ml-2 rounded-full shrink-0 overflow-hidden
+      border border-gray-medium bg-gray-ultra-light"
+      >
+        <span
+          v-if="imageHasError || !contact.image"
+          class="font-medium uppercase"
+        >{{ nameAbbrv }}
+        </span>
+
+        <img
+          v-else
+          class="object-cover"
+          :src="contact.image"
+          alt="contact-logo"
+          @error="imageHasError = true"
+          @load="imageHasError = false"
+        >
+      </div>
     </div>
 
-    <div class="flex text-sm font-medium text-gray-dark border-t border-gray-ultra-light">
-      <div class="flex items-center justify-center flex-1 py-4 cursor-pointer hover:text-gray">
-        <IconEnvelope />
-        <span class="ml-3">Email</span>
-      </div>
-      <div
-        class="flex items-center justify-center flex-1 py-4 border-l
-            border-gray-ultra-light cursor-pointer hover:text-gray"
-      >
-        <IconPhone />
-        <span class="ml-3">Call</span>
-      </div>
+    <div class="flex justify-end mt-2 gap-2">
+      <template v-if="editMode">
+        <span
+          class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+          @click.stop="editMode = false"
+        >Cancel</span>
+
+        <span
+          class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+          @click.stop="onSave"
+        >Save</span>
+      </template>
+
+      <template v-else>
+        <span
+          class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+          @click.stop="triggerEditMode"
+        >Edit</span>
+
+        <span
+          class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
+          @click.stop="$emit('delete', contact)"
+        >Delete</span>
+      </template>
     </div>
-  </div>
+
+    <template #footer>
+      <div class="flex text-sm font-medium text-gray-dark border-t border-gray-ultra-light" @click.stop>
+        <div class="flex items-center justify-center flex-1 py-4 cursor-pointer hover:text-gray">
+          <IconEnvelope />
+          <span class="ml-3">Email</span>
+        </div>
+        <div
+          class="flex items-center justify-center flex-1 py-4 border-l
+            border-gray-ultra-light cursor-pointer hover:text-gray"
+        >
+          <IconPhone />
+          <span class="ml-3">Call</span>
+        </div>
+      </div>
+    </template>
+  </Card>
 </template>
 
 <script lang="ts" setup>
-import { ref, nextTick } from 'vue'
+import { ref, nextTick, computed } from 'vue'
 import type { IContact } from '@/types'
 import IconEnvelope from '@/components/icons/IconEnvelope.vue'
 import IconPhone from '@/components/icons/IconPhone.vue'
+import Card from '@/components/Card.vue'
 
 const props = defineProps<{
   contact: IContact
@@ -88,6 +106,15 @@ const localContact = ref<Omit<IContact, 'id'>>({
   image: ''
 })
 
+const nameAbbrv = computed(() => {
+  return props.contact.name.split(' ').reduce((acc, cur) => {
+    if (acc.length < 2) {
+      acc = acc.concat(cur[0])
+    }
+    return acc
+  }, '')
+})
+
 const editMode = ref(false)
 
 async function triggerEditMode () {
@@ -101,4 +128,6 @@ function onSave () {
   emit('save', localContact.value)
   editMode.value = false
 }
+
+const imageHasError = ref(false)
 </script>
